2f02d897927f0b2d7edd3bd249db6a66aa5e3a96
iron man <33849449+wangyn1998@users.noreply.github.com>
2020-04-15 11:21:27 +0800

    发现页更新

--- 前端/src/discover/Discover.js
+++ 前端/src/discover/Discover.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import Swiper from 'react-native-swiper';
+import {
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    Image,
+    AsyncStorage,
+    ToastAndroid,
+    BackHandler
+  } from 'react-native';
+
+const styles = StyleSheet.create({
+    wrapper:{
+        height:250,
+        width:"100%"
+    },
+    body:{
+        height:400,
+        width:'100%',
+        marginTop:50,
+        backgroundColor:'red'
+    }
+})
 
 export default class Discover extends Component {
     render() {
         return (
             <View>
-                <Text> Discover </Text>
+                <ScrollView>
+                    <View style={styles.wrapper}>
+                        <Swiper>
+                            <View >
+                                <Image source={require('../../images/lb1.png')} style={{width:"100%",height:"100%"}} />
+                            </View>
+                            <View >
+                                <Image source={require('../../images/lb2.png')} style={{width:"100%",height:"100%"}} />
+                            </View>
+                        </Swiper>
+                    </View>
+                    <View style={styles.body}>
+                        
+                    </View>
+                </ScrollView>
+                
             </View>
         )
     }
