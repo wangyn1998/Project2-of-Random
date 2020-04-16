177df291e30c205107ef7db75df2a4abed9ff492
zhangyujuan123 <48017498+zhangyujuan123@users.noreply.github.com>
2020-04-16 12:08:28 +0800

    修改学习盒

--- 前端/App.js
+++ 前端/App.js
@@ -23,6 +23,9 @@ import Personal from './src/my/Personal';
 import Signin from './src/my/Signin';
 import Xian from './src/my/Xian';
 import Bing from './src/my/Bing';
+import Selection from './src/box/Selection';
+import Test from './src/box/Test';
+import Learn from './src/box/Learn';
 const styles = StyleSheet.create({
 
 })
@@ -158,7 +161,10 @@ const App = () => {
                     />
                 </Scene>
               </Tabs>
+              <Scene key='test' component={Test} title='测试' titleStyle={{textAlign:'center',flex:1,color:'#ffffff'}} renderRightButton={<Icon name='ellipsis' color='#ffffff' size='lg' style={{marginRight:20}}/>} navigationBarStyle={{backgroundColor:'#79be3b',color:'#ffffff'}} navBarButtonColor='#ffffff' />
+              <Scene key='learn' component={Learn} title='全部卡片' titleStyle={{textAlign:'center',flex:1,color:'#ffffff'}} renderRightButton={<Icon name='ellipsis' color='#ffffff' size='lg' style={{marginRight:20}}/>} navigationBarStyle={{backgroundColor:'#79be3b',color:'#ffffff'}} navBarButtonColor='#ffffff' />
             </Scene>
+            <Scene key='light' component={Selection}/>
         </Lightbox>
         <Scene  key="login" component={Login} initial={true}/>
         <Scene key="register" component={Register}/>
