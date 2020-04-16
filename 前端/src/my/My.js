5f2448f94659b56d9aa09a287bc59f5dc21e5371
fym <33849795+fym1@users.noreply.github.com>
2020-04-16 10:52:10 +0800

    fym

--- 前端/src/my/My.js
+++ 前端/src/my/My.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-import {View,Text,Image,TextInput,TouchableOpacity, AsyncStorage, StyleSheet,Alert,ScrollView} from 'react-native'
+import {View,Text,Image,TextInput,TouchableOpacity, AsyncStorage, StyleSheet,Alert,ScrollView, FlatList} from 'react-native'
 import { Icon } from '@ant-design/react-native'
 import { Actions } from 'react-native-router-flux'
+import Xian from './Xian';
+const boxes = [
+    {name:'盒子',num:6,img:require('../../images/my-box1.png'),unit:'个',txtcolor:'#ff8e14'},
+    {name:'卡片',num:6,img:require('../../images/my-box2.png'),unit:'个',txtcolor:'#67e5fb'},
+    {name:'积分',num:6,img:require('../../images/my-box3.png'),unit:'分',txtcolor:'#fed2df'},
 
+]
 export default class My extends Component {
     render() {
         return (
             <ScrollView style={{backgroundColor:'#fff',width:'100%',height:'100%'}}>
-                <View style={{flex:1,position:'relative'}}>
+                <View style={{flex:1,alignItems:'center'}}>
                     <View style={styles.introduce}>
                         <View style={styles.touxiang}>
                             <Image source={require('../../images/touxiang.png')} style={{width:80,height:80}}/>
@@ -20,8 +26,55 @@ export default class My extends Component {
                             </TouchableOpacity>
                         </View>
                     </View>
-                    <View style={styles.boxes}>
-
+                    <View style={styles.bigbox}>
+                        <FlatList
+                            data = {boxes}
+                            numColumns = {3}
+                            renderItem={({item})=>(
+                                <View style={styles.boxes}>
+                                    <Text style={{color:`${item.txtcolor}`,fontSize:17}}>{item.num}{item.unit}</Text>
+                                    <Image style={styles.boximg}
+                                            resizeMode="contain"
+                                            source={item.img}
+                                    />
+                                    <Text style={styles.boxname}>{item.name}</Text>
+                                </View>
+                            )}
+                        />
+                        <TouchableOpacity onPress={()=>{
+                                Actions.signin()}}
+                                style={styles.sign}
+                        >
+                            <Text style={{color:'#79be3b'}}>签到领积分></Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.studystu}>
+                        <Text style={{color:'#79be3b',fontSize:15}}>我的学习情况</Text>
+                        <View style={styles.tu}>
+                            <TouchableOpacity onPress={()=>{Actions.xian()}}
+                                style={{
+                                    width:'35%',
+                                    height:'100%',
+                                    justifyContent:'center',
+                                    alignItems:'center'
+                                }}
+                            >
+                                <Image source={require('../../images/my-xian.png')}/>
+                                <Text>线形图</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={()=>{Actions.bing()}}
+                                style={{
+                                    marginLeft:'20%',
+                                    width:'35%',
+                                    height:'100%',
+                                    justifyContent:'center',
+                                    alignItems:'center'
+                                }}
+                            >
+                                <Image source={require('../../images/my-bing.png')}/>
+                                <Text>饼状图</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
             </ScrollView>
@@ -54,15 +107,57 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
         marginTop:'7%'
     },
-    boxes:{
+    bigbox:{
         width:'80%',
-        height:200,
+        height:170,
         borderRadius:30,
-        backgroundColor:'red',
+        backgroundColor:'#fff',
         borderWidth:2,
         borderColor:'#eee',
-        position:'absolute',
-        top:110,
-        left:'9.5%'
+        marginTop:-80,
+    },
+    boxes:{
+        width:'25%',
+        height:120,
+        marginLeft:'7%',
+        marginTop:10,
+        paddingTop:10,
+        alignItems:'center',
+        // backgroundColor:'red'
+    },
+    boximg:{
+        width:50,
+        height:50
+    },
+    boxname:{
+        fontSize:15,
+        color:'#aaa'
+    },
+    sign:{
+        borderWidth:1,
+        borderColor:'#79be3b',
+        width:'25%',
+        borderRadius:5,
+        alignItems:'center',
+        marginBottom:10,
+        marginLeft:'70%'
+    },
+    studystu:{
+        width:'90%',
+        height:170,
+        borderRadius:30,
+        backgroundColor:'#fff',
+        borderWidth:3,
+        borderColor:'#79be3b',
+        marginTop:20,
+        alignItems:'center',
+    },
+    tu:{
+        width:'90%',
+        height:'80%',
+        // backgroundColor:'red',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
     }
 })
