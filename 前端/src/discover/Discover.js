c8587418b76697c4fe641e453dcdc63264ff15a9
iron man <33849449+wangyn1998@users.noreply.github.com>
2020-04-16 11:00:43 +0800

    修改发现

--- 前端/src/discover/Discover.js
+++ 前端/src/discover/Discover.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from 'react-native-button';
 import Swiper from 'react-native-swiper';
 import {
     StyleSheet,
@@ -17,10 +18,53 @@ const styles = StyleSheet.create({
         width:"100%"
     },
     body:{
-        height:400,
+        height:420,
         width:'100%',
         marginTop:50,
-        backgroundColor:'red'
+        flexDirection:'row',
+        flexWrap:'wrap'
+    },
+    game:{
+        height:180,
+        width:'30%',
+        marginLeft:'15%',
+        marginRight:'5%',
+        marginBottom:30,
+        backgroundColor:'#00C2B2'
+    },
+    knowledge:{
+        height:180,
+        width:'30%',
+        marginLeft:'5%',
+        marginRight:'15%',
+        marginBottom:30,
+        backgroundColor:'#FFF162'
+    },
+    everyday:{
+        height:180,
+        width:'30%',
+        marginLeft:'15%',
+        marginRight:'5%',
+        marginBottom:30,
+        backgroundColor:'#FED2DF'
+    },
+    test:{
+        height:180,
+        width:'30%',
+        marginLeft:'5%',
+        marginRight:'15%',
+        marginBottom:30,
+        backgroundColor:'#B114E8'
+    },
+    txt1:{
+        marginTop:70,
+        fontSize:30,
+        marginLeft:'30%'
+    },
+    txt2:{
+        marginTop:70,
+        fontSize:30,
+        marginLeft:'10%'
     }
 })
 
@@ -40,7 +84,26 @@ export default class Discover extends Component {
                         </Swiper>
                     </View>
                     <View style={styles.body}>
-                        
+                        <View style={styles.game}>
+                            <Button>
+                                <Text style={styles.txt1}>游戏</Text>
+                            </Button>
+                        </View>
+                        <View style={styles.knowledge}>
+                            <Button>
+                                <Text style={styles.txt1}>知识</Text>
+                            </Button>
+                        </View>
+                        <View style={styles.everyday}>
+                            <Button>
+                                <Text style={styles.txt2}>每日推荐</Text>
+                            </Button>
+                        </View>
+                        <View style={styles.test}>
+                            <Button>
+                                <Text style={styles.txt2}>考试信息</Text>
+                            </Button>
+                        </View>
                     </View>
                 </ScrollView>
                 
