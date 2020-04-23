36ada3ce4916e9c0bc801b9d2f1c8c280b0fa5cf
fym <33849795+fym1@users.noreply.github.com>
2020-04-23 22:03:54 +0800

    Add files via upload

--- 前端/src/block/Block.js
+++ 前端/src/block/Block.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View ,TouchableOpacity} from 'react-native'
+import { Actions } from 'react-native-router-flux'
 export default class Block extends Component {
     render() {
         return (
             <View>
-                <Text> Block </Text>
+                <TouchableOpacity onPress={()=>{Actions.send()}}>
+                    <Text>发帖子</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>{Actions.sended()}}>
+                    <Text>我发出的</Text>
+                </TouchableOpacity>
             </View>
         )
     }
