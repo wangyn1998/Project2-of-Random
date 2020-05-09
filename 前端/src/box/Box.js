867c192248ab025df382ba8d4d5cc24e2f2f5413
zhangyujuan123 <48017498+zhangyujuan123@users.noreply.github.com>
2020-05-09 21:14:34 +0800

    update box

--- 前端/src/box/Box.js
+++ 前端/src/box/Box.js
@@ -3,30 +3,30 @@ import { Text, View, FlatList,TextInput, Image, TouchableOpacity, ScrollView,Dim
 import { Grid, Icon,SearchBar } from '@ant-design/react-native';
 import {Actions} from 'react-native-router-flux'
 
-const list=[
+var list=[
     {
-        num:'41',
+        num:0,
         num2:'天',
         img:'clock-circle',
         text:'学习',
         color:'#FD8F71'
     },
     {
-        num:'5',
+        num:0,
         num2:'次',
         img:'snippets',
         text:'打卡',
         color:'#34C7FC'
     },
     {
-        num:'6',
+        num:0,
         num2:'个',
         img:'star',
         text:'星星',
         color:'#FFD331'
     },
     {
-        num:'4',
+        num:0,
         num2:'座',
         img:'trophy',
         text:'成就',
@@ -35,46 +35,84 @@ const list=[
 ]
 const list1=[
     {
-        num:'41',
-        img:require('../../images/box-1.png'),
-        text:'英语'
-    },
-    {
-        num:'41',
-        img:require('../../images/box-1.png'),
-        text:'英语'
-    },
-    {
-        num:'41',
-        img:require('../../images/box-1.png'),
-        text:'英语'
-    },
-    {
-        num:'41',
-        img:require('../../images/box-1.png'),
-        text:'数学'
-    },
-    {
-        num:10,
-        img:require('../../images/box-jia.png'),
+        img:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADEAAAAzCAYAAAA+VOAXAAACEUlEQVRoge2ZS0sCURTHzzx8ZYYpQi83plBEiwKDatO2RZta9AVa1r5tn6GW9QnatGtTEEQQLRIyahE9KCpqelhU2uho3gsazVyV0atdh/sDUe4Bz/8HM3fOzAi5PNDkiP8dgAZcghW4BCvI9fjTD/UVvrVPSGfV30aiDeyiE9ocAer9qEukMp+wdbkGl4lDUL6ui+t+Vzd0tIZhum8RXLKHak/qEvHHbTi42zCsPydv8SfkHYbRnhmqPamfE6dPu2XrZy/7tFvSl1C1VE31arDE7sQlWIFLsAKXYAUuwQqWkCg5AKpaEjL5UTqby1JvikZ1s9gkBzikFmKNKIHGaTSNoqkT/TYDkq9UR6O6Wbo8EejzjxHvRwT9I5tsToO9m3XYPF8x3ajeDAYmYHZgCURB+rNuOCfevhUmBRBxZQfn02OQeE3eNyRQtZDyWWJ3MkjojzfWIOUzSHidAXDK7oYEMosk2nA+PQSJToh2TWERQWDnaEN5wu1RnE8P8Tox2TuPv68Sx5DWvkw1e0ndl71W2CUX+AhBKhHxjxRz6TFcJ2plNbYAF4lYyXrIOwRzQ8s0W1p0d2pGuAQrcAlW4BKswCVYgUuQsImOmurVQF0i7IuWvB+RRTuu04b6i8fx4Cy8qwocPWyDlssU10VBhqCnH9dpQ30UL3Ci7GKZAm67Dz9yqQd1k2gkfHdiBUtI/ACe6Z+VsrbPGgAAAABJRU5ErkJggg==",
         text:'+'
     }
 ]
 const {width,scale,height}=Dimensions.get('window');
 const w=width,h=height;
+var list2=[];
 export default class Box extends Component {
     constructor(){
         super();
         this.state={
             text:'',
-            data:list1
+            data:list1,
+            list:list
         }
+    }
+    async componentDidMount(){
+        await fetch('http://172.17.100.2:3000/users/record')
+            .then((res)=>res.json())
+            .then((res)=>{
+                var list=[
+                    {
+                        num:res[0].recordDay,
+                        num2:'天',
+                        img:'clock-circle',
+                        text:'学习',
+                        color:'#FD8F71'
+                    },
+                    {
+                        num:res[0].recordClockIn,
+                        num2:'次',
+                        img:'snippets',
+                        text:'打卡',
+                        color:'#34C7FC'
+                    },
+                    {
+                        num:res[0].recordStars,
+                        num2:'个',
+                        img:'star',
+                        text:'星星',
+                        color:'#FFD331'
+                    },
+                    {
+                        num:res[0].recordAchievement,
+                        num2:'座',
+                        img:'trophy',
+                        text:'成就',
+                        color:'#FD84AB'
+                    }
+                ]
+                this.setState({
+                    list:list
+                })
+            })
+            await fetch('http://172.17.100.2:3000/users/box')
+                .then((res)=>res.json())
+                .then((res)=>{
+                    for(var i=0;i<res.length;i++){
+                        var obj={
+                            text:res[i].boxName,
+                            img:res[i].boxImg
+                        }
+                        list2.push(obj);
+                    }
+                    list2.push({
+                        img:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADEAAAAzCAYAAAA+VOAXAAACEUlEQVRoge2ZS0sCURTHzzx8ZYYpQi83plBEiwKDatO2RZta9AVa1r5tn6GW9QnatGtTEEQQLRIyahE9KCpqelhU2uho3gsazVyV0atdh/sDUe4Bz/8HM3fOzAi5PNDkiP8dgAZcghW4BCvI9fjTD/UVvrVPSGfV30aiDeyiE9ocAer9qEukMp+wdbkGl4lDUL6ui+t+Vzd0tIZhum8RXLKHak/qEvHHbTi42zCsPydv8SfkHYbRnhmqPamfE6dPu2XrZy/7tFvSl1C1VE31arDE7sQlWIFLsAKXYAUuwQqWkCg5AKpaEjL5UTqby1JvikZ1s9gkBzikFmKNKIHGaTSNoqkT/TYDkq9UR6O6Wbo8EejzjxHvRwT9I5tsToO9m3XYPF8x3ajeDAYmYHZgCURB+rNuOCfevhUmBRBxZQfn02OQeE3eNyRQtZDyWWJ3MkjojzfWIOUzSHidAXDK7oYEMosk2nA+PQSJToh2TWERQWDnaEN5wu1RnE8P8Tox2TuPv68Sx5DWvkw1e0ndl71W2CUX+AhBKhHxjxRz6TFcJ2plNbYAF4lYyXrIOwRzQ8s0W1p0d2pGuAQrcAlW4BKswCVYgUuQsImOmurVQF0i7IuWvB+RRTuu04b6i8fx4Cy8qwocPWyDlssU10VBhqCnH9dpQ30UL3Ci7GKZAm67Dz9yqQd1k2gkfHdiBUtI/ACe6Z+VsrbPGgAAAABJRU5ErkJggg==",
+                        text:'+'
+                    });
+                    this.setState({
+                        data:list2
+                    })
+                })
     }
     submit=(text)=>{
         var arr=[];
-        for(var i=0;i<list1.length;i++){
-            if(list1[i].text==text){
-                arr.push(list1[i]);
+        for(var i=0;i<list2.length;i++){
+            if(list2[i].text==text){
+                arr.push(list2[i]);
             }
         }
         this.setState({
@@ -84,7 +122,7 @@ export default class Box extends Component {
     clearText=(text)=>{
         if(text==''){
             this.setState({
-                data:list1
+                data:list2
             })
         }
     }
@@ -94,7 +132,7 @@ export default class Box extends Component {
                 <ScrollView>
                 <View style={{flexDirection:'row',justifyContent:'space-around',paddingTop:10,paddingBottom:10,borderColor:'#BBBBBB',borderWidth:1,borderBottomLeftRadius:10,borderBottomRightRadius:10,backgroundColor:'#FFFFFF'}}>
                 {
-                    list.map((item)=>(
+                    this.state.list.map((item)=>(
                         <View style={{alignItems:'center',width:'25%'}}>
                             <View style={{flexDirection:'row'}}><Text style={{color:item.color}}>{item.num}</Text><Text style={{color:'#807B7B'}}>{item.num2}</Text></View>
                             <Icon name={item.img} style={{marginTop:10,marginBottom:10}} color={item.color}/>
@@ -115,13 +153,13 @@ export default class Box extends Component {
                         if(item.text=='+'){
                             return (
                                 <View style={{width:'50%',padding:10,alignItems:'center',justifyContent:'center',height:h*0.2}}>
-                                    <TouchableOpacity onPress={()=>{Actions.addBox()}}><Image source={item.img} style={{width:w*0.2,height:h*0.1}}/></TouchableOpacity>
+                                    <TouchableOpacity onPress={()=>{Actions.addBox()}}><Image source={{uri:item.img}} style={{width:w*0.2,height:0.1*h}}/></TouchableOpacity>
                                 </View>
                             )
                         }
                         return (
-                            <TouchableOpacity onPress={()=>{Actions.light()}} style={{alignItems:'center',width:'50%',padding:10}}>
-                                    <Image source={item.img}/>
+                            <TouchableOpacity onPress={()=>{Actions.light();Actions.refresh()}} style={{alignItems:'center',width:'50%',padding:10}}>
+                                    <Image source={{uri:item.img}} style={{height:h*0.18,width:'65%'}}/>
                                     <Text style={{color:'#E81414',marginTop:10}}>{item.text}</Text>
                             </TouchableOpacity>
                         )
