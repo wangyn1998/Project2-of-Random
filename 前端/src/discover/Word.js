60bd7b7030fdd65b6e22fbad6688ff2ee07a3cf0
iron man <33849449+wangyn1998@users.noreply.github.com>
2020-05-18 14:21:05 +0800

    完善游戏组件

--- 前端/src/discover/Word.js
+++ 前端/src/discover/Word.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import WebView from 'react-native-webview'
 
 export default class Word extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={{weight:'100%',height:1000}}>
+               <WebView style={{weight:'100%',height:1000}} source={{ uri: 'http://www.4399.com/flash/200761_2.htm' }} /> 
             </View>
         )
     }
