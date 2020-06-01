fc5410fb8da76c9c835d3eaa4fdb098724e7fc9d
fym <33849795+fym1@users.noreply.github.com>
2020-06-01 17:10:41 +0800

    Add files via upload

--- 前端/src/block/Block.js
+++ 前端/src/block/Block.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View ,Image,TouchableOpacity,ScrollView, StyleSheet,FlatList} from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import {Icon} from '@ant-design/react-native';
 const styles = StyleSheet.create({
     kuang:{
         width:'100%',
@@ -53,13 +54,20 @@ const styles = StyleSheet.create({
         color:'#bbb',
         fontSize:15,
         fontWeight:'bold',
-    }
+    },
+    btn2:{
+        flexDirection:'row',
+        alignItems:'center',
+        marginLeft:'15%'
+    },
 })
 export default class Block extends Component {
     constructor(){
         super();
         this.state={
-            posts:[]
+            posts:[],
+            iszan:false,
+            result:[],
         }
     }
     componentDidMount(){
@@ -67,25 +75,89 @@ export default class Block extends Component {
         .then((res)=>res.json())
         .then((res)=>{
             // console.log(res);
+            let result = [];
+            for(let index in res){
+                result.push({postId: res[index].postId, iszan: false});
+            }
             this.setState({
                 posts:res,
+                result:result
             })
         })
     }
-    post=(item)=>{
-        Actions.blockMsg({xiangqing:item});
-        let text = {postId:item.postId} //获取数据
-        let send = JSON.stringify(text);   //重要！将对象转换成json字符串
-        fetch(`http://172.17.100.2:3000/users/clicknum`,{   //Fetch方法y
-            method: 'POST',
-            headers: {'Content-Type': 'application/json; charset=utf-8'},
-            body: send
-        })
-        .then(res => res.json())
-        .then(
-            data => {
+    zan=(postPointNumber,postId)=>{
+        let len = this.state.result.length;
+        for(let index = 0;index < len;index++){
+            if(postId == this.state.result[index].postId){
+                console.log(this.state.result[index].postId);
+                if(this.state.result[index].iszan == false){
+                    let zan = postPointNumber+1;
+                    let text = {postPointNumber:zan,postId:postId} //获取数据
+                    let send = JSON.stringify(text);   //重要！将对象转换成json字符串
+                    fetch('http://172.17.100.2:3000/users/zan',{   //Fetch方法y
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json; charset=utf-8'},
+                        body: send
+                    })
+                    .then(res => res.json())
+                    .then(
+                        res => {
+                            if(res.success){
+                                fetch('http://172.17.100.2:3000/users/block')
+                                .then((res)=>res.json())
+                                .then((res)=>{
+                                    var jieguo = this.state.result;
+                                    jieguo[index].iszan = true;
+                                    console.log(jieguo)
+                                    this.setState({
+                                        result:jieguo,
+                                        posts:res,
+                                    })
+                                })
+                            }
+                            else{
+                                console.log('111')
+                            }
+                        }
+                    )
+                }
+                else{
+                    let zan = postPointNumber-1;
+                    let text = {postPointNumber:zan,postId:postId} //获取数据
+                    let send = JSON.stringify(text);   //重要！将对象转换成json字符串
+                    fetch('http://172.17.100.2:3000/users/zan',{   //Fetch方法y
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json; charset=utf-8'},
+                        body: send
+                    })
+                    .then(res => res.json())
+                    .then(
+                        res => {
+                            if(res.success){
+                                fetch('http://172.17.100.2:3000/users/block')
+                                .then((res)=>res.json())
+                                .then((res)=>{
+                                    var jieguo1 = this.state.result;
+                                    jieguo1[index].iszan = false;
+                                    console.log(jieguo1);
+                                    this.setState({
+                                        result:jieguo1,
+                                        posts:res,
+                                    })
+                                })
+                               
+                            }
+                            else{
+                                console.log('111')
+                            }
+                        }
+                    )
+                }
             }
-        )
+            else{
+                console.log('111')
+            }
+        }
     }
     render() {
         return (
@@ -96,8 +168,8 @@ export default class Block extends Component {
                             data = {this.state.posts}
                             numColumns = {1}
                             style={{width:'97%'}}
-                            renderItem={({item})=>(
-                                <TouchableOpacity style={styles.kuang} onPress={()=>{this.post(item)}}>
+                            renderItem={({item,index})=>(
+                                <TouchableOpacity style={styles.kuang} onPress={()=>{Actions.blockMsg({xiangqing:item})}}>
                                     <View style={styles.user}>
                                         <View style={styles.touxiang}>
                                             <Image source={{uri:item.userImage}} style={{width:80,height:80}}/>
@@ -122,10 +194,37 @@ export default class Block extends Component {
                                             <Image source={require('../../images/block-pinglun.png')}/>
                                             <Text style={styles.numtxt}>{item.postReplyNum}</Text>
                                         </TouchableOpacity>
-                                        <TouchableOpacity style={{flexDirection:'row',alignItems:'center',marginLeft:'28%'}}>
+                                        {/* <TouchableOpacity style={{flexDirection:'row',alignItems:'center',marginLeft:'28%'}} onPress={()=>this.zan(postPointNumber=item.postPointNumber,postId=item.postId)}>
                                             <Image source={require('../../images/block-zan.png')}/>
                                             <Text style={styles.numtxt}>{item.postPointNumber}</Text>
-                                        </TouchableOpacity>
+                                        </TouchableOpacity> */}
+                                        {
+                                            [1].map(()=>{
+                                                console.log(this.state.result[index])
+                                                if(this.state.result[index].iszan){
+                                                    return (
+                                                        <TouchableOpacity 
+                                                            style={{flexDirection:'row',alignItems:'center',marginLeft:'28%'}} 
+                                                            onPress={()=>this.zan(postPointNumber=item.postPointNumber,postId=item.postId)}
+                                                        >
+                                                            <Icon name='like' color='#79be3b' size={35}/>
+                                                            <Text style={{color:'#79be3b',marginLeft:'2%',fontSize:15,fontWeight:'bold'}}>{item.postPointNumber}</Text>
+                                                        </TouchableOpacity>
+                                                    )
+                                                }else if(!this.state.result[index].iszan){
+                                                    console.log('false')
+                                                    return (
+                                                        <TouchableOpacity  
+                                                            style={{flexDirection:'row',alignItems:'center',marginLeft:'28%'}} 
+                                                            onPress={()=>this.zan(postPointNumber=item.postPointNumber,postId=item.postId)}
+                                                        >
+                                                            <Icon name='like' color='#bbb' size={35}/>
+                                                            <Text style={{color:'#bbb',marginLeft:'2%',fontSize:15,fontWeight:'bold'}}>{item.postPointNumber}</Text>
+                                                        </TouchableOpacity>
+                                                    )
+                                                }
+                                            })
+                                        }
                                     </View>
                                 </TouchableOpacity>
                             )}
